429662b93bc4af78df0bcb2ad6373f085d81b4cc
Sergio Costas <raster@rastersoft.com>
2013-08-23 00:54:28 +0200

    Mas limpieza de codigo

--- driver.c
+++ driver.c
@@ -10,23 +10,11 @@
 
 #include "driver.h"
 
-#define GSLX680_I2C_NAME 	"gslX680"
 #define GSLX680_I2C_ADDR 	0x40
-#define IRQ_PORT			RK30_PIN4_PC2
-#define WAKE_PORT		    RK30_PIN4_PD0
 
 #define GSL_DATA_REG		0x80
 #define GSL_STATUS_REG		0xe0
 #define GSL_PAGE_REG		0xf0
-
-#define PRESS_MAX  			255
-#define MAX_FINGERS			10
-#define MAX_CONTACTS 		10
-#define DMA_TRANS_LEN		0x20
-
-#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
-
-#define swap(x, y) do { typeof(x) z = x; x = y; y = z; } while (0)
 
 char *fw_file;
 
@@ -35,41 +23,6 @@ struct i2c_client {
 	int adapter;
 	int flags;
 };
-
-struct gsl_ts {
-	struct i2c_client *client;
-	struct workqueue_struct *wq;
-	struct gsl_ts_data *dd;
-	u8 *touch_data;
-	u8 device_id;
-	u8 prev_touches;
-	bool is_suspended;
-	bool int_pending;
-	int irq;
-};
-
-struct gsl_ts_data {
-	u8 x_index;
-	u8 y_index;
-	u8 z_index;
-	u8 id_index;
-	u8 touch_index;
-	u8 data_reg;
-	u8 status_reg;
-	u8 data_size;
-	u8 touch_bytes;
-	u8 update_data;
-	u8 touch_meta_data;
-	u8 finger_size;
-};
-
-
-static inline u16 join_bytes(u8 a, u8 b) {
-	u16 ab = 0;
-	ab = ab | a;
-	ab = ab << 8 | b;
-	return ab;
-}
 
 
 static int gslX680_shutdown_low(void) {
@@ -132,7 +85,7 @@ static void reset_chip(struct i2c_client *client) {
 	u8 buf[1];
 	
 	buf[0]=0x88;
-	gsl_ts_write(client, 0xe0, buf, 1);
+	gsl_ts_write(client, GSL_STATUS_REG, buf, 1);
 	usleep(10000);
 
 	buf[0]=0x04;
@@ -156,7 +109,7 @@ static void reset_chip(struct i2c_client *client) {
 
 static void gsl_load_fw(struct i2c_client *client) {
 
-	u8 buf[DMA_TRANS_LEN*4 + 1] = {0};
+	u8 buf[9] = {0};
 	u32 source_line = 0;
 	int retval;
 
@@ -197,7 +150,7 @@ static void gsl_load_fw(struct i2c_client *client) {
 
 static void startup_chip(struct i2c_client *client) {
 	u8 tmp = 0x00;
-	gsl_ts_write(client, 0xe0, &tmp, 1);
+	gsl_ts_write(client, GSL_STATUS_REG, &tmp, 1);
 	usleep(10000);	
 }
 
@@ -227,7 +180,7 @@ void read_coords(struct i2c_client *cliente) {
 	unsigned int x,y,total;
 	unsigned int vx,vy;
 
-	retval=gsl_ts_read(cliente, 0x80, buffer, 1);
+	retval=gsl_ts_read(cliente, GSL_DATA_REG, buffer, 1);
 	if (retval<=0) {
 		printf("error leyendo coordenadas %d\n",retval);
 		return;
@@ -245,7 +198,7 @@ void read_coords(struct i2c_client *cliente) {
 	y=(((unsigned int)buffer[2])+256*((unsigned int)buffer[3]));
 	vx=(x>>12)&0x000F;
 	vy=(y>>12)&0x000F;
-	printf("Pulsacion en %dx%d, vx %d (%d pulsaciones)\r",x&0x0FFF,y&0x0FFF,vx,total);
+	printf("Pulsacion en %dx%d, vx %d, vy %d (%d pulsaciones)\r",x&0x0FFF,y&0x0FFF,vx,vy,total);
 }
 
 
@@ -253,16 +206,14 @@ int main(int argc, char **argv) {
 
 	struct i2c_client cliente;
 	
-	if (argc!=4) {
-		printf("Format: driver DEVICE ADDRESS FW_FILE\n");
+	if (argc!=3) {
+		printf("Format: driver DEVICE FW_FILE\n");
 		return 0;
 	}
 
-	int addr = atoi(argv[2]); /* The I2C address */
+	fw_file=argv[2]; // firmware file
 	
-	fw_file=argv[3]; // firmware file
-	
-	printf("Connecting to device %s, address %d, firmware %s\n",argv[1],addr,fw_file);
+	printf("Connecting to device %s, firmware %s\n",argv[1],fw_file);
 	
 	cliente.adapter=open(argv[1],O_RDWR);
 	if (cliente.adapter<0) {
@@ -270,15 +221,15 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	cliente.addr=addr;
+	cliente.addr=GSLX680_I2C_ADDR;
 
 	system("echo 0 > /sys/devices/virtual/misc/sun4i-gpio/pin/pb3");
 	sleep(1);
 	system("echo 1 > /sys/devices/virtual/misc/sun4i-gpio/pin/pb3");
 
-	if (ioctl(cliente.adapter, I2C_SLAVE, addr) < 0) {
+	if (ioctl(cliente.adapter, I2C_SLAVE, GSLX680_I2C_ADDR) < 0) {
 		/* ERROR HANDLING; you can check errno to see what went wrong */
-		printf("Error selecting device %d\n",addr);
+		printf("Error selecting device %d\n",GSLX680_I2C_ADDR);
 		return -2;
 	}
 
